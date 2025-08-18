c4ad98deebfbe735e076bf5ed50c0e41c1618e7d
jude <ndubuisijude46@gmail.com>
2025-08-18 13:29:41 +0100

    created a file direction.py

---
+++ assignment/direction.py
@@ -0,0 +1,15 @@
+"""
+This module provides functions to determine the direction of movement based on user input as stated.
+"""
+
+
+# def get_direction():
+#     direction = input("Enter a direction (up, down, left, right): ").lower()
+#     if direction in ['up', 'down', 'left', 'right']:
+#         return direction
+#     else:
+#         print("Invalid direction. Please try again.")
+#         return get_direction()
+
+
+
