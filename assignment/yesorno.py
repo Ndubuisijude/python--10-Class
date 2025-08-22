8687e91994909e58170220aa04b547699c1bb0e6
jude <ndubuisijude46@gmail.com>
2025-08-22 15:08:40 +0100

    assignment/yesorno.py

--- assignment/yesorno.py
+++ assignment/yesorno.py
@@ -7,3 +7,22 @@ while True:
         continue
     elif cont == "no":
         break
+
+# correctiion
+"""
+simple yes or no program
+"""
+#user choice-input
+
+choice = input("do you want to continue: enter any key to continue or no to stop?")
+
+#loop
+while(choice == "y"):
+    print("alright continuing the program")
+    choice = input("do you want to continue: y or no?")
+    if(choice == "y"):
+        continue
+    elif(choice == "n"):
+        break
+    else:
+        print("invalid input")
