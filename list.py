ac22880defaf309e13ab3d9b4fc3bb859e3b4522
jude <ndubuisijude46@gmail.com>
2025-08-25 13:08:17 +0100

    new file created list.py

---
+++ list.py
@@ -0,0 +1,31 @@
+name1 = "kamsi"
+name2 = "justina"
+name3 = "despina"
+
+#            0           1           2
+classNames = ["kamsi", "justina", "despina", "ohakwe", "nenye", "chidimma"]
+
+print(len(classNames)) # length of the list
+# print the values acording to the index
+print(classNames[0])
+print(classNames[1])
+print(classNames[2])
+
+# name1 = input("Enter your name: ")
+# height = float(input("Enter your height: "))
+
+# gender = input("Enter your gender: ")
+# isMarried = input("Are you married? (True/False): ")
+
+myDetails = ["kamsi", 5.6, "female", False]
+
+print(type(myDetails))
+# con = list((name1, height, gender, isMarried))
+
+print(myDetails[-4]) #negative indexing
+# #method
+# con.append
+
+# #function
+# len(con)
+# print(type(con))
